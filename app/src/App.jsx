997f678ace940955e2d12dbdc215733785bded61
Developers Today <mshari7185@gmail.com>
2024-06-25 23:28:35 +0530

    app component

---
+++ app/src/App.jsx
@@ -0,0 +1,13 @@
+// frontend/src/App.js
+import React from 'react';
+import RecordingsList from './components/RecordingsList';
+function App() {
+  return (
+    <div className="App">
+      <RecordingsList />
+    </div>
+  );
+}
+
+export default App;
+
